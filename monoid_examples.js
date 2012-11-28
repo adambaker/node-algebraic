3686f898b77ae398c8942b7fa904a995e19b8db0
Adam C. Baker <adam.durandal@gmail.com>
2012-11-28 02:31:36 -0800

    min, max, product monoids.

--- monoid_examples.js
+++ monoid_examples.js
@@ -4,6 +4,9 @@ var qc = require('quickcheck');
 var proto;
 
 var Monoid = m.Monoid;
+var inf = Number.POSITIVE_INFINITY;
+
+function arbNum() { return Math.log(Math.abs(qc.arbDouble())+1); }
 
 function Sum (val) {
   this.val = val;
@@ -16,13 +19,47 @@ proto.eq  = function(other) {
     Math.abs(1 - this.val/other.val) < Sum.eqDelta;
 };
 proto.toString = function() { return this.val; };
-Monoid(Sum, {id: new Sum(0), arb: function() {return new Sum(qc.arbInt()/10)} });
+Monoid(Sum, {id: new Sum(0), arb: function() {return new Sum(arbNum())} });
 exports.Sum = Sum;
 
+function Product(val) {
+  this.val = val;
+}
+Product.eqDelta = 1e-10;
+proto = Product.prototype;
+proto.dot = function(other) { return new Product(this.val * other.val) };
+proto.eq  = function(other) {
+  return this.val === other.val ||
+    Math.abs(1 - this.val/other.val) < Product.eqDelta;
+};
+proto.toString = function() { return this.val; };
+Monoid(Product, { id: new Product(1), arb: function() {return new Product(arbNum());} });
+exports.Product = Product;
 
+
+function Max(val) {
+  this.val = val;
+}
+proto = Max.prototype;
+proto.dot = function(other) { return this.val > other.val ? this : other; };
+proto.eq  = function(other) { return this.val === other.val };
+proto.toString = function() { return this.val; };
+Monoid(Max, { id: new Max(-inf), arb: function() {return new Max(arbNum());} });
+exports.Max = Max;
+
+function Min(val) {
+  this.val = val;
+}
+proto = Min.prototype;
+proto.dot = function(other) { return this.val < other.val ? this : other; };
+proto.eq  = function(other) { return this.val === other.val };
+proto.toString = function() { return this.val; };
+Monoid(Min, { id: new Min(inf), arb: function() {return new Min(arbNum());} });
+exports.Min = Min;
 function Any (val) {
   this.val = val;
 }
+
 proto = Any.prototype;
 proto.dot = function(other) { return new Any(this.val || other.val) };
 proto.eq  = function(other) { return this.val === other.val };
