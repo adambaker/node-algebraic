89b470e5c26fac29ef8d3ad1bcd6b9bd5bb03bc3
Adam C. Baker <adam.durandal@gmail.com>
2012-11-30 22:36:27 -0800

    extract value wrapping code.

--- monoid/instances.js
+++ monoid/instances.js
@@ -9,31 +9,38 @@ var inf = Number.POSITIVE_INFINITY;
 function arbNum() { return Math.log(Math.abs(qc.arbDouble())+1); }
 function identOrEq(a, b) { return a === b || (a.eq && a.eq(b)) }
 
+function ValWrapper() {}
+proto = ValWrapper.prototype;
+proto.toString = function() { return this.val; };
+proto.eq  = function(other) { return this.val === other.val };
+
+
 function Sum (val) {
   this.val = val;
 }
 Sum.eqDelta = 1e-10;
+Sum.prototype = new ValWrapper(); Sum.prototype.constructor = Sum;
 proto = Sum.prototype;
 proto.dot = function(other) { return new Sum(this.val + other.val) };
 proto.eq  = function(other) {
   return this.val === other.val ||
     Math.abs(1 - this.val/other.val) < Sum.eqDelta;
 };
-proto.toString = function() { return this.val; };
 Monoid(Sum, {id: new Sum(0), arb: function() {return new Sum(arbNum())} });
 exports.Sum = Sum;
+
 
 function Product(val) {
   this.val = val;
 }
 Product.eqDelta = 1e-10;
+Product.prototype = new ValWrapper(); Product.prototype.constructor = Product;
 proto = Product.prototype;
 proto.dot = function(other) { return new Product(this.val * other.val) };
 proto.eq  = function(other) {
   return this.val === other.val ||
     Math.abs(1 - this.val/other.val) < Product.eqDelta;
 };
-proto.toString = function() { return this.val; };
 Monoid(Product, { id: new Product(1), arb: function() {return new Product(arbNum());} });
 exports.Product = Product;
 
@@ -41,52 +48,51 @@ exports.Product = Product;
 function Max(val) {
   this.val = val;
 }
-proto = Max.prototype;
-proto.dot = function(other) { return this.val > other.val ? this : other; };
-proto.eq  = function(other) { return this.val === other.val };
-proto.toString = function() { return this.val; };
+Max.prototype = new ValWrapper(); Max.prototype.constructor = Max;
+Max.prototype.dot = function(other) { return this.val > other.val ? this : other; };
 Monoid(Max, { id: new Max(-inf), arb: function() {return new Max(arbNum());} });
 exports.Max = Max;
+
 
 function Min(val) {
   this.val = val;
 }
-proto = Min.prototype;
-proto.dot = function(other) { return this.val < other.val ? this : other; };
-proto.eq  = function(other) { return this.val === other.val };
-proto.toString = function() { return this.val; };
+Min.prototype = new ValWrapper(); Min.prototype.constructor = Min;
+Min.prototype.dot = function(other) { return this.val < other.val ? this : other; };
 Monoid(Min, { id: new Min(inf), arb: function() {return new Min(arbNum());} });
 exports.Min = Min;
 
+
 function Any (val) {
-this.val = val;
+  this.val = val;
 }
-proto = Any.prototype;
-proto.dot = function(other) { return new Any(this.val || other.val) };
+Any.prototype = new ValWrapper(); Any.prototype.constructor = Any;
+Any.prototype.dot = function(other) { return new Any(this.val || other.val) };
 proto.eq  = function(other) { return this.val === other.val };
-proto.toString = function() { return this.val; };
 Monoid(Any, {id: new Any(false), arb: function() {return new Any(qc.arbBool())} });
 exports.Any = Any;
 
+
 function All (val) {
-this.val = val;
+  this.val = val;
 }
-proto = All.prototype;
-proto.dot = function(other) { return new All(this.val && other.val) };
+All.prototype = new ValWrapper(); All.prototype.constructor = All;
+All.prototype.dot = function(other) { return new All(this.val && other.val) };
 proto.eq  = function(other) { return this.val === other.val };
-proto.toString = function() { return this.val; };
 Monoid(All, {id: new All(true), arb: function() {return new All(qc.arbBool())} });
 exports.All = All;
+
 
 proto = String.prototype;
 proto.dot = function(other) { return this + other };
 proto.eq  = function(other) { return this.valueOf() === other.valueOf() };
 Monoid(String, {id: '', arb: qc.arbString });
 
+
 proto = Array.prototype;
 proto.dot = function(other) { return this.concat(other) }
 proto.eq  = function(other) {
-return this.length === other.length && this.every(function(v, i){return identOrEq(v, other[i])});
+  return this.length === other.length && this.every(function(v, i){return identOrEq(v, other[i])});
 }
 Monoid(Array, {id: [], arb: function(){return qc.arbArray(qc.arbByte)}});
 
@@ -110,6 +116,7 @@ Object.arb = function() {
   return { a: Sum.arb(), b: All.arb(), randop_prop: String.arb(), stuff: Array.arb() };
 }
 Monoid(Object, {id: {}});
+
 
 function add_log_key(logs, key, value) {
   if( value.monoid === Object && value instanceof Function )
